7ff87c6674870e63ca9f56bd5538ac0a6b179bd6
Taichi Sato <okms310gmail.com>
2022-02-06 14:52:58 +0900

    sampleを削除してきれいにする

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { dateFormat } from './dateformat';
 import { QuickStartContainer1Provider, QuickStartContainer1TreeElement } from './quickstartContainer1Provider';
 
 // this method is called when your extension is activated
@@ -9,91 +8,15 @@ import { QuickStartContainer1Provider, QuickStartContainer1TreeElement } from '.
 export function activate(context: vscode.ExtensionContext) {
 	const quickstartContainer1Provider = new QuickStartContainer1Provider();
 	vscode.window.registerTreeDataProvider('quickstartContainer1', quickstartContainer1Provider);
-	vscode.commands.registerCommand('quickstartContainer1.show', (element: QuickStartContainer1TreeElement) => {
+	const showDisposable = vscode.commands.registerCommand('quickstartContainer1.show', (element: QuickStartContainer1TreeElement) => {
 		if (element) {
 			vscode.window.showInformationMessage(`This is ${element.name}`, { modal: true });
 		}
 	});
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "quickstart" is now active!');
-
-	let show_disposable = vscode.commands.registerCommand('quickstart.show', message => {
-		if (message) {
-			vscode.window.showInformationMessage(message);
-		}
-	});
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('quickstart.helloWorld', async () => {
-		// vscode.commands.executeCommand("quickstart.show", "HelloWorld")
-		// open テスト
-		// let uri = vscode.Uri.file('/Users/Teach/workspace');
-		// // workspaceとして開くコマンド
-		// let success = await vscode.commands.executeCommand('vscode.openFolder', uri);
-		// console.log(success);
-		// 永続化用のパスを開くテスト
-		// let uri = context.storageUri
-		// console.log(`Workspace Storage Path: ${uri?.path}`)
-		await onCommandInputBox1(context)
-	});
-
-	let disposable_gettoday = vscode.commands.registerCommand('quickstart.getToday', () => {
-		let today: Date = new Date();
-		vscode.window.showInformationMessage("Today:" + dateFormat(today))
-	});
-
 	context.subscriptions.push(
-		disposable,
-		disposable_gettoday,
-		show_disposable
+		showDisposable
 	);
-}
-
-// ラベルなし選択肢
-const onCommandInputTest1 = async (context: vscode.ExtensionContext) => {
-	const selectedItem = await vscode.window.showQuickPick(
-		['Red', 'Green', 'Blue', 'Yellow'],
-		{
-			canPickMany: false,
-			placeHolder: 'Choose your favorite color'
-		})
-	if (selectedItem) {
-		vscode.window.showInformationMessage(`You choose ${selectedItem}`);
-	}
-}
-
-// ラベルあり
-const onCommandInputTest2 = async (context: vscode.ExtensionContext) => {
-	// labelがメインの方
-	const actions: vscode.QuickPickItem[] = [
-		{ label: 'Action1', description: 'Description of Action1' },
-		{ label: 'Action2', description: 'Description of Action2' },
-		{ label: 'Action3', description: 'Description of Action3' },
-		{ label: 'Action4', description: 'Description of Action4' }
-	]
-	const selectedItem = await vscode.window.showQuickPick(
-		actions,
-		{
-			canPickMany: false,
-			placeHolder: 'Choose your favorite action'
-		})
-	if (selectedItem) {
-		vscode.window.showInformationMessage(`You choose ${selectedItem.label}`);
-	}
-}
-
-const onCommandInputBox1 = async (context: vscode.ExtensionContext) => {
-	const inputString = await vscode.window.showInputBox({
-		prompt: 'Input your name',
-		validateInput: (s: string): string | undefined => (!s) ? 'You must input something!' : undefined
-	})
-	if (inputString) {
-		vscode.window.showInformationMessage(`Your name is ${inputString}`);
-	}
 }
 
 // this method is called when your extension is deactivated
--- src/test/suite/gettoday.test.ts
+++
@@ -1,9 +0,0 @@
-import * as assert from 'assert';
-import { dateFormat } from '../../dateformat';
-
-suite('Extension Test Suite', () => {
-	test('Dateformat test', () => {
-		let testdate: Date = new Date("2020-1-1");
-		assert.strictEqual(dateFormat(testdate), '2020-1-1');
-	});
-});
